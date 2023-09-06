f5cc0e54f758579b11c1e7a09e9bc7b16fa4bd18
DiegoSCRocha <diegoscr.ec@gmail.com>
2023-09-06 15:38:06 -0300

    lynx
    comit

---
+++ APITesteLynx/APITesteLynx/Controllers/Lynxontroller.cs
@@ -0,0 +1,52 @@
+﻿using APITesteLynx.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace APITesteLynx.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class Lynxontroller : Controller
+    {
+        
+        [HttpGet(Name = "GetRandom")]
+        public double Get()
+        {
+
+            Random rnd = new Random();
+            return rnd.NextDouble();
+
+        }
+
+        [HttpPost]
+        public async Task<float> PostDTO([FromBody]DTO pDto)
+        {
+
+            float resultado;
+
+          
+
+
+            switch (pDto.operacao)
+            {
+                case "*": resultado = pDto.PrimeiroValor * pDto.SegundoValor;
+                    break;
+                case "+": resultado = pDto.PrimeiroValor + pDto.SegundoValor;
+                    break;
+                case "-":
+                    resultado = pDto.PrimeiroValor - pDto.SegundoValor;
+                    break;
+                case "/":
+                    resultado = pDto.PrimeiroValor / pDto.SegundoValor;
+                    break;
+                default:
+                    resultado = pDto.PrimeiroValor + pDto.SegundoValor;              
+                  break;
+            }
+
+
+            return resultado;
+        }
+    }
+}
---
+++ APITesteLynx/APITesteLynx/Models/DTO.cs
@@ -0,0 +1,16 @@
+﻿namespace APITesteLynx.Models
+{
+    public class DTO
+    {
+
+        public DateTime Data { get; set; }
+        public string Nome { get; set; }
+
+        public float PrimeiroValor { get; set; }
+        public float SegundoValor { get; set; }
+        public String operacao { get; set; }
+       
+
+
+    }
+}
